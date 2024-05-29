2ba28da75cd6bd260c86a23593db05c616ab3477
hajimemashite <arikuiteam0914@gmail.com>
2024-05-29 18:28:25 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// 高さを100vhピッタリに
 
+// 高さをデバイスの画面ピッタリに
 const fullHeight = () => {
   const vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -46,15 +46,63 @@ gsap.to(indicatorCounter, {
   }
 });
 
+// aタグホバー
+const links = document.querySelectorAll("a");
+
+links.forEach((link) => {
+  link.addEventListener('mousemove', () => {
+    gsap.to(link, { 
+      duration: 1,
+      delay: 0,
+      color: "var(--hover-color)",
+    });
+  });
+  link.addEventListener('mouseleave', () => {
+    gsap.to(link, {
+      duration: 1,
+      delay: 0,
+      color: "var(--main-color)",
+    });
+  });
+});
+
+// リストホバー
+const workInners = document.querySelectorAll(".work-inner");
+
+workInners.forEach((workInner) => {
+  const lines = workInner.querySelector(".work-hover-line");
+  const sign = workInner.querySelector(".link-sign");
+  const title = workInner.querySelector(".title");
+  
+  workInner.addEventListener('mouseenter', () => {
+    gsap.to(title, { 
+      duration: 0.05,
+      delay: 0,
+      marginLeft: "2rem",
+      // ease: "expo.in",
+    });
+  });
+
+  workInner.addEventListener('mouseleave', () => {
+    gsap.to(title, { 
+      duration: 0.05,
+      delay: 0,
+      marginLeft: "0rem",
+      // ease: "power2.out",
+    });
+  });
+});
+
+
+// カラムの幅を取得
 function updateColmnWidth() {
-  var column = document.getElementsByClassName('grid-column')[0]; // 最初の要素を取得
+  var column = document.getElementsByClassName('grid-column')[0];
   if (column) {
     var newWidth = column.offsetWidth;
     document.documentElement.style.setProperty("--column-width", `${newWidth}px`);
     console.log(newWidth)
-  }
-}
+  };
+};
 // 初期読み込み時に幅を設定
 updateColmnWidth();
-// リサイズイベントリスナーを追加
 window.addEventListener('resize', updateColmnWidth);
